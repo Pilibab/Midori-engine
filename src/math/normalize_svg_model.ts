4f8009cbfbae5ccdd99a7a512cb84fc72445c23c
Prometheus <toshiyasato219@gmail.com>
2026-03-13 18:00:33 +0900

    fix: correct import path and enhance normalize_model function

--- src/math/normalize_svg_model.ts
+++ src/math/normalize_svg_model.ts
@@ -1,22 +1,43 @@
 
 import { sampleDataKanji } from "../test-data/sample-kanji.ts";
 import type { StrokeComponent } from "../types.ts";
-import { extract_svg_path } from "@/helper/extract_svg_path.ts";
+import { extract_svg_path } from "../helper/extract_svg_path.ts";
 
 import { svgPathProperties } from "svg-path-properties";
 
-
-const normalize_model = (svg: StrokeComponent[]) => {
-
-    const paths = extract_svg_path(svg)
-
-
+interface pointsVal {
+    x: number;
+    y: number;
+}
+const normalize_model = (svg: StrokeComponent[], numPoints: number = 100) => {
+    // numPoints tells us how many points are we going to use for the full kanji
+    // the higher the number the more accurate
+    // think of using points to graph something
+    const paths = extract_svg_path(svg);
+    const points: pointsVal[] = [];
     
-    console.log(paths);
+    const properties = paths.map((path, _) => {
+        // extracts the svg
+        const svgAtt = new svgPathProperties(path);
 
+        const totalLength = svgAtt.getTotalLength();
 
+        for (let i = 0; i < numPoints; i++) {
+            const frac = i / numPoints;
+
+            const point = svgAtt.getPointAtLength(frac * totalLength);
+        
+            // append point
+            points.push({
+                x: Math.round(point.x * 100) / 100, // Round for clean data
+                y: Math.round(point.y * 100) / 100
+            });
+        }
+    });
+    console.log(points);
+    return points;
 }
 
-normalize_model(sampleDataKanji.stroke_components)
+// normalize_model(sampleDataKanji.stroke_components)
 
 

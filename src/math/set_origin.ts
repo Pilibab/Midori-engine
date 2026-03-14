3444f000f588bc73b0f46e1525823d6142c1282a
Prometheus <toshiyasato219@gmail.com>
2026-03-14 11:36:05 +0900

    feat: implemented logic that makes the first point the origin

---
+++ src/math/set_origin.ts
@@ -0,0 +1,23 @@
+// this file is for setting the first point of the stroke as the origin so that both stroke will have the same starting point 
+import type { pointsVal } from "../types.ts";
+
+const set_origin = (points: pointsVal[]) => {
+    // if points is singular return 
+    if (points.length == 0) return 
+
+    // get the x, y of the first point 
+    const {x: shiftX, y: shiftY} = points[0]
+
+    const shiftedPoints = points.map((point, _) => {
+        const {x, y} = point;
+
+        // export shifter points 
+        return {
+            x: x - shiftX,
+            y: y - shiftY
+        }
+    })
+    return shiftedPoints;
+} 
+
+export default set_origin;
